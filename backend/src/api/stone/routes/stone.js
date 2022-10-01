7aa346d22c4f48a90448e2a80c4f939b29f6f67c
urfaust <karakaslidis@yahoo.gr>
2022-10-01 14:52:53 +0300

    Backend

---
+++ backend/src/api/stone/routes/stone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stone router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::stone.stone');
---
+++ backend/src/api/stone/services/stone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stone service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::stone.stone');
